31ca8d2af4b570e97c141e04ac6504bba00e97c7
DiarmuiF <dirmuid.f@gmail.com>
2022-10-12 22:18:02 +0100

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
 const sumAll = function() {
 
+    const args = Array.from(arguments).sort()
+    const start = args[0];
+    const stop = args[1]
+
+    let output = 0
+
+    if (start < 0 || stop < 0 || typeof start != 'number' || typeof stop != 'number') {
+        return "ERROR"
+    }
+
+    for (i = start; i <= stop; i++) {
+
+        output += i
+
+    }
+
+    return output
+
 };
 
 // Do not edit below this line
