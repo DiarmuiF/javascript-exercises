19dd809e59ddc8f5d2395af3a3029233c61ec7a2
DiarmuiF <dirmuid.f@gmail.com>
2022-10-12 21:56:25 +0100

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
 
+    args = Array.from(arguments);
+
+    array = args[0]
+    elements = args.slice(1);
+
+    for (let i = 0; i < elements.length; i++) {
+
+        let location = array.indexOf(elements[i]);
+
+        if (location > -1){
+
+            array.splice(location, 1);
+        
+        }
+    }
+
+    return array
+
 };
 
 // Do not edit below this line
